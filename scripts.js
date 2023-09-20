2c058011af39fe57b58895e08cbdebae9337ebdc
Eduardo Lorensetti Lopes <eduardolorensetti@gmail.com>
2023-09-20 08:01:23 -0300

    1.0

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+    const inputCurrencyValue = document.querySelector(".s-converter")
+    console.log(inputCurrencyValue)
+}
+
+convertButton.addEventListener("click", convertValues)
